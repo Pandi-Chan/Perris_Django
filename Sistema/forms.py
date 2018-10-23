66b76781411327fb646698ea0fdffb2aa1351da4
John Molina Becker <42702401+Pandi-Chan@users.noreply.github.com>
2018-10-23 17:48:28 -0300

    CSS added to Registro.html

--- Sistema/forms.py
+++ Sistema/forms.py
@@ -1,11 +1,13 @@
 from django import forms
 
 class RegistrarPersona(forms.Form):
-    rutPersona=forms.CharField(widget=forms.TextInput(),label="Rut: ")
-    passwordPersona=forms.CharField(widget=forms.PasswordInput(),label="Contraseña: ")
-    nombrePersona=forms.CharField(widget=forms.TextInput(),label="Nombre: ")
-    apellidoPersona=forms.CharField(widget=forms.TextInput(),label="Apellido: ")
-    #fechaNacimiento=forms.CharField(widget=forms.DateField(),label="Fecha Nacimiento: ")
-    direccionPersona=forms.CharField(widget=forms.TextInput(),label="Direccion: ")
-    numeroFono=forms.CharField(widget=forms.TextInput(),label="Telefono: ")
-    #mailPersona=forms.EmailField(widget=forms.EmailField(),label="Email: ")
+    rutPersona=forms.CharField(widget=forms.TextInput(),label="Rut")
+    passwordPersona=forms.CharField(widget=forms.PasswordInput(),label="Contraseña")
+    nombrePersona=forms.CharField(widget=forms.TextInput(),label="Nombre")
+    apellidoPersona=forms.CharField(widget=forms.TextInput(),label="Apellido")
+    #        Arreglar FECHA NACIMIENTO
+    fechaNacimiento=forms.DateField(widget=forms.DateInput(),label="Fecha de Nacimiento")
+    #-------------------------------------
+    direccionPersona=forms.CharField(widget=forms.TextInput(),label="Direccion")
+    numeroFono=forms.CharField(widget=forms.TextInput(),label="Telefono")
+    mailPersona=forms.EmailField(label="Email: ")
